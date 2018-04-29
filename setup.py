8f5d1ceb49ccaad826ecafcf895d6b9565eec0ca
nedel <yaroslav@gmx.it>
2018-04-29 21:00:30 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='monypy',
+      version='0.1a',
+      description='async lightweight ODM for mongodb',
+      url='http://github.com/nede1/monypy',
+      author='Yaroslav Unknown',
+      author_email='yaroslav@gmx.it',
+      license='MIT',
+      packages=['motor'],
+      install_requires=['motor'],
+      zip_safe=False)
