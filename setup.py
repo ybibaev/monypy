f17238f0fcdc50a91004cad41f896fc76dab85b5
nedel <yaroslav@gmx.it>
2018-05-05 13:44:54 +0300

    improve setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='monypy',
-      version='0.1a',
-      description='async lightweight ODM for mongodb',
-      url='http://github.com/nede1/monypy',
-      author='Yaroslav Unknown',
-      author_email='yaroslav@gmx.it',
-      license='MIT',
-      packages=['monypy'],
-      install_requires=['motor'],
-      zip_safe=False)
+VERSION = '0.1'
+
+setup(
+    name='monypy',
+    version=VERSION,
+    description='Asynchronous lightweight ODM for mongodb',
+    keywords='asyncio mongodb',
+    url='http://github.com/nede1/monypy',
+
+    author='Yaroslav Unknown',
+    author_email='yaroslav@gmx.it',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 4 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries'
+    ],
+
+    packages=find_packages(exclude=['tests*']),
+    install_requires=['motor>=1.2.0'],
+    setup_requires=['pytest-asyncio', 'flake8'],
+    tests_require=['pytest-asyncio']
+)
