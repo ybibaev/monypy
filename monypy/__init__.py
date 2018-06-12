65438bb4d435f0fa89ccf665e363cc286b0b9b6d
Yaroslav Unknown <yaroslav@gmx.it>
2018-06-12 21:22:36 +0300

    bump version

--- monypy/__init__.py
+++ monypy/__init__.py
@@ -2,7 +2,7 @@ from .doc import Doc
 from .exceptions import DocumentDoesNotExistError, DocumentInitDataError
 from .manager import Manager
 
-__version__ = '1.0'
+__version__ = '1.0.1'
 
 __all__ = (
     'Doc',
