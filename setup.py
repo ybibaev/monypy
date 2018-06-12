f0ca7f07337ff908a649cfe04f8892a55bc3e17d
Yaroslav Unknown <yaroslav@gmx.it>
2018-06-12 19:08:01 +0300

    added improvements to setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,18 @@ from setuptools import setup, find_packages
 
 import monypy
 
+with open('README.md') as fh:
+    long_description = fh.read()
+
+
 setup(
     name='monypy',
     version=monypy.__version__,
     description='Asynchronous lightweight ODM for mongodb',
     keywords='asyncio mongodb',
     url='http://github.com/nede1/monypy',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 
     author='Yaroslav Unknown',
     author_email='yaroslav@gmx.it',
@@ -15,6 +21,8 @@ setup(
     license='MIT',
 
     classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: AsyncIO',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Topic :: Software Development',
