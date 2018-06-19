b435f75094344b066e09b505539e7ebc91be7fc1
Yaroslav Unknown <yaroslav@gmx.it>
2018-06-19 08:52:29 +0300

    inc version

--- monypy/__init__.py
+++ monypy/__init__.py
@@ -2,7 +2,7 @@ from .doc import Doc
 from .exceptions import DocumentDoesNotExistError, DocumentInitDataError
 from .manager import Manager
 
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 
 __all__ = (
     'Doc',
