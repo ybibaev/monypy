c2a06c83b836fdd39648217e7b6253128a9f77c2
Yaroslav Unknown <yaroslav@gmx.it>
2018-06-22 19:09:00 +0300

    metaclass refactor

--- monypy/meta.py
+++ monypy/meta.py
@@ -39,41 +39,46 @@ class DocMeta(type):
         cls = super().__new__(mcs, name, bases, clsargs)
 
         db = get_database(loop, **database_attrs)
-        collection = create_db_collection(cls, db, collection_attrs)
+        collection = get_collection(cls, db, collection_attrs)
 
         cls.manager = manager_factory(cls, collection)
 
         return cls
 
     def __call__(cls, *args, **kwargs):
+        instance = super().__call__()
+
+        init_data = cls.get_init_data(*args, **kwargs)
+        default_init_data = cls.get_default_init_data()
+        instance.__dict__[DOC_DATA] = {**default_init_data, **init_data}
+
+        cls.prepare_instance_data(instance)
+        return instance
+
+    def get_init_data(cls, *args, **kwargs):
         if args and kwargs:
             raise DocumentInitDataError('Only arg or only kwargs')
 
-        init_data = copy.deepcopy(
-            args[0] if args else kwargs
-        )
-        check_init_data(init_data)
+        init_data = copy.copy(args[0] if args else kwargs)
+        cls.check_init_data(init_data)
+        return init_data
 
-        default_init_data = copy.deepcopy(
-            cls.__dict__.get(DOC_INIT_DATA, {})
-        )
-        check_init_data(default_init_data)
+    def get_default_init_data(cls):
+        default_init_data = copy.copy(cls.__dict__.get(DOC_INIT_DATA, {}))
+        cls.check_init_data(default_init_data)
+        return default_init_data
 
-        instance = super().__call__()
-        defaults = instance.__dict__[DOC_DATA] = {
-            **default_init_data, **init_data
-        }
+    @staticmethod
+    def check_init_data(init_data):
+        if not isinstance(init_data, dict):
+            raise DocumentInitDataError('Init data must be an instance of dict')
 
-        for k, v in defaults.items():
+    @staticmethod
+    def prepare_instance_data(instance):
+        init_data = instance.__dict__[DOC_DATA]
+        for k, v in init_data.items():
             if callable(v):
-                defaults[k] = v(instance)
-
-        return instance
-
-
-def check_init_data(init_data):
-    if not isinstance(init_data, dict):
-        raise DocumentInitDataError('Init data must be an instance of dict')
+                init_data[k] = v(instance)
 
 
 def get_database(loop, **database_attrs):
@@ -82,6 +87,19 @@ def get_database(loop, **database_attrs):
     attrs.update(io_loop=loop)
     client = connect(**attrs)
     return client[db_name]
+
+
+def get_collection(doc_class, db, data):
+    try:
+        collection_name = data['name']
+    except TypeError:
+        collection_name = doc_class.__name__.lower()
+
+    collection_codec_options = DEFAULT_CODEC_OPTIONS \
+        .with_options(document_class=doc_class)
+
+    return db[collection_name] \
+        .with_options(collection_codec_options)
 
 
 def manager_factory(doc_class, collection):
@@ -106,15 +124,3 @@ def find(classes, token):
     with suppress(StopIteration):
         return next(filter(bool, map(_find, classes)))
 
-
-def create_db_collection(doc_class, db, data):
-    try:
-        collection_name = data['name']
-    except TypeError:
-        collection_name = doc_class.__name__.lower()
-
-    collection_codec_options = DEFAULT_CODEC_OPTIONS \
-        .with_options(document_class=doc_class)
-
-    return db[collection_name] \
-        .with_options(collection_codec_options)
